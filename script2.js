bc32ee2c5286612898c559c98ed3c250037886e7
brainiac1010 <armanmahim00@gmail.com>
2024-06-01 19:47:49 +0600

    fix the nagitavi balance and NaN proble

--- script2.js
+++ script2.js
@@ -1,42 +1,31 @@
-// Handle deposit
-document.getElementById('btn-deposit').addEventListener('click', function () {
-    const depositField = document.getElementById('deposit-field');
-    const newDepositAmountString = depositField.value;
-    const newDepositAmount = parseFloat(newDepositAmountString);
 
-    const depositTotalElement = document.getElementById('deposit-total');
-    const previousDepositTotalString = depositTotalElement.innerText;
-    const previousDepositTotal = parseFloat(previousDepositTotalString);
-
-    const currentDepositTotal = previousDepositTotal + newDepositAmount;
-    depositTotalElement.innerText = currentDepositTotal;
-
-    const balanceTotal = document.getElementById('balance-total');
-    const previousBalanceTotalString = balanceTotal.innerText;
-    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
-
-    const currentBalanceTotal = previousBalanceTotal + newDepositAmount;
-    balanceTotal.innerText = currentBalanceTotal;
-
-    depositField.value = '';
-});
-
-// Handle withdrawal
 document.getElementById('btn-withdraw').addEventListener('click', function () {
     const withdrawField = document.getElementById('withdraw-field');
     const newWithdrawStrAmount = withdrawField.value;
     const newWithdrawAmount = parseFloat(newWithdrawStrAmount);
 
+   
+    if (isNaN(newWithdrawAmount)) {
+        alert('Please provide a valid number');
+        return; 
+    }
+
     const withdrawTotalElement = document.getElementById('withdraw-total');
     const previousWithdrawStr = withdrawTotalElement.innerText;
     const previousWithdraw = parseFloat(previousWithdrawStr);
 
-    const currentWithdrawTotal = previousWithdraw + newWithdrawAmount;
-    withdrawTotalElement.innerText = currentWithdrawTotal;
-
     const balanceTotalElement = document.getElementById('balance-total');
     const previousBalanceTotalString = balanceTotalElement.innerText;
     const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+    // Check if the withdrawal amount exceeds the current balance
+    if (newWithdrawAmount > previousBalanceTotal) {
+        alert("Insufficient balance");
+        return; 
+    }
+
+    const currentWithdrawTotal = previousWithdraw + newWithdrawAmount;
+    withdrawTotalElement.innerText = currentWithdrawTotal;
 
     const finalBalance = previousBalanceTotal - newWithdrawAmount;
     balanceTotalElement.innerText = finalBalance;
